0f89b9d01328fc6efd5716e5b9c2e8dad8cf93bd
ColeyG <cjgeerts@gmail.com>
2020-05-07 00:16:09 -0400

    Added Palettes | Added Head

---
+++ src/html.js
@@ -0,0 +1,38 @@
+// This is a react component standin for the document head
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+        <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,300;0,500;1,500&family=Noto+Sans+SC&family=Ubuntu+Mono&display=swap" rel="stylesheet"></link>
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={'body'}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
