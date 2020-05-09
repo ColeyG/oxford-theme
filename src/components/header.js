a360a09d94a66f557325d1628cfb295ee99f1534
ColeyG <cjgeerts@gmail.com>
2020-05-08 21:08:13 -0400

    Header

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <header>
+      <header className="bc-primary-header">
         <h1>{this.props.mainTitle}</h1>
       </header>
     );
