a5d8c1276434eb4e80a03aa731fe2d2088199c15
ColeyG <cjgeerts@gmail.com>
2020-05-12 00:24:38 -0400

    Header Nav |

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 class Header extends React.Component {
   render() {
     return (
       <header className="cl-primary-header">
-        <h1>{this.props.mainTitle}</h1>
+        <Link className="head-logo" to="/">
+          <h1>{this.props.mainTitle}</h1>
+        </Link>
+        <nav>
+          <ul>
+            <li><a href="#">Case Studies</a></li>
+            <li><a href="#">Resume</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Work</a></li>
+          </ul>
+        </nav>
       </header>
     );
   }
