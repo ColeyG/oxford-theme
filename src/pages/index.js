fa1f43a7670e37d3fc338115acb885b1b5368f1c
ColeyG <cjgeerts@gmail.com>
2020-05-15 19:23:47 -0400

    Jumbotron Files | Changed sizing of pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/header';
+import Jumbotron from '../components/jumbotron';
 import Footer from '../components/footer';
 import '../../styles/main.scss';
 
 export default () => <div className="cl-wrapper">
   <div className="cl-page">
     <Header mainTitle="Cole Geerts" />
+    <Jumbotron />
   </div>
   <Footer />
 </div>;
