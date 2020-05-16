3a82d601a778578bfdbcdb6db81b0db84bd340e7
ColeyG <cjgeerts@gmail.com>
2020-05-16 01:13:57 -0400

    Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,14 @@ import React from 'react';
 class Footer extends React.Component {
   render() {
     return (
-      <footer className="cl-primary-footer"></footer>
+      <footer className="cl-primary-footer">
+        <div className="left">
+          <p>Cole Geerts &copy; 2016 - 2020</p>
+        </div>
+        <div className="right">
+          <a href="#" className="cl-a-reverse">github</a>
+        </div>
+      </footer>
     );
   }
 }
