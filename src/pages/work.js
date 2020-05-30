5950044b77a419ac46a399f6cc6f637cb934e24b
ColeyG <cjgeerts@gmail.com>
2020-05-30 00:58:05 -0400

    Add Remaining Pages

---
+++ src/pages/work.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+export default () => (<div className="cl-wrapper">
+  <div className="cl-page">
+    <Header mainTitle="Cole Geerts" />
+    <div>
+      <h2>Work</h2>
+    </div>
+  </div>
+  <Footer />
+</div>);
