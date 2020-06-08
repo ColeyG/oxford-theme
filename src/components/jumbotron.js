56ac1ff6924dd500ab0d372772aca267861ec3a9
ColeyG <cjgeerts@gmail.com>
2020-06-08 00:28:31 -0400

    Jumbotron Spacing

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -6,13 +6,12 @@ class Jumbotron extends React.Component {
     return (
       <div className={`cl-jumbotron ${this.props.exClassName}`}>
         <div className="content">
-          <h2>Hi! I'm Cole Geerts and I'm a Web Developer out of London Ontario. You can find some important links like those to my
-            <a href="https://github.com/ColeyG" target="_blank" rel="noopener noreferrer" className="cl-a-primary">github</a>
-             and
-            <Link to="/resume" className="cl-a-secondary">web resume</Link> here.
-             Otherwise if you've landed here from somewhere else you can check out my
-            <Link to="/blog" className="cl-a-tertiary">blog</Link>
-              for notes on things I am particularly interested in and assorted rants.
+          <h2>
+            Hi! I'm Cole Geerts and I'm a Web Developer out of London Ontario. You can find some important links like those to
+            my <a href="https://github.com/ColeyG" target="_blank" rel="noopener noreferrer" className="cl-a-primary">github</a> and <Link to="/resume" className="cl-a-secondary">web resume</Link> here.
+            Otherwise if you've landed here from somewhere else you can check out
+            my <Link to="/blog" className="cl-a-tertiary">blog</Link> for notes on things
+            I am particularly interested in and assorted rants.
           </h2>
         </div>
         <div className="interactive">
