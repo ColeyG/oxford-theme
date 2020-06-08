3bcc520d0be03f3b05e49bf1c4db19aa360e3fc3
ColeyG <cjgeerts@gmail.com>
2020-06-08 13:17:15 -0400

    Compressing

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+#
+# This script requires the ImageMagick photo transformer.
+#
+
 basedir=$(pwd)/assets
 
 for file in $basedir/raw/*;
 do
   filename=$(echo "$file" | sed 's/raw/optimized/g')
   echo $filename
-  convert -strip -interlace Plane -gaussian-blur 0.05 -quality 85% $file $filename
+  convert -strip -interlace Plane -gaussian-blur 0.5 -quality 70% $file $filename
 done
