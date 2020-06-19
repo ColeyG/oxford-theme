3113e321decf782c12a97d6e60197b87640a04cd
ColeyG <cjgeerts@gmail.com>
2020-06-19 16:07:12 -0400

    Adding Stars Start

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,10 +3,29 @@ import './styles/main.scss';
 const canvas = document.querySelector('.header-canvas');
 const c = canvas.getContext('2d');
 
+let starArray = [];
+let amountOfStars = 10;
+
+for (let i; amountOfStars <= i; i++) {
+  // TODO: Assoc Array here. Currently x, y, radius
+  starArray.push([10, 10, 50]);
+}
+
+const starRender = (star) => {
+  c.beginPath();
+  c.arc(star[0], star[1], star[2], 0, 2 * Math.PI, false);
+  c.fillStyle = 'green';
+  c.fill();
+};
+
 const animateLoop = () => {
   requestAnimationFrame(animateLoop);
   canvas.width = window.innerWidth;
   canvas.height = canvas.offsetHeight;
+
+  starArray.forEach((star) => {
+    starRender(star);
+  });
 };
 
 animateLoop();
