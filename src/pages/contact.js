93aa6de96a6d08c244809c6dafe6b038c552e5f3
ColeyG <cjgeerts@gmail.com>
2020-06-26 12:10:28 -0400

    Contact Form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,17 @@ export default () => (<div className="cl-wrapper">
         <div className="contact-cta"></div>
       </div>
       <div className="contact-section contact-secondary">
-        <div className="contact-form"></div>
+        <div className="contact-form">
+          <form action="" className='cl-form'>
+            <label htmlFor="name" className="cl-label">Name</label>
+            <input className="cl-input" type="text" name="name" id="name" />
+            <label htmlFor="email" className="cl-label">Email</label>
+            <input className="cl-input" type="text" name="email" id="email" />
+            <label htmlFor="message" className="cl-label">Enter a Message</label>
+            <textarea className="cl-textarea" name="" id="" cols="30" rows="10" name="message" id="message"></textarea>
+            <button className="cl-btn-primary">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
