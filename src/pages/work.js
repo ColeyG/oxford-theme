fbb12d410008600d307c58c13f1d93352b936937
ColeyG <cjgeerts@gmail.com>
2020-07-03 16:40:47 -0400

    Work Commits

--- src/pages/work.js
+++ src/pages/work.js
@@ -5,8 +5,12 @@ import Footer from '../components/footer';
 export default () => (<div className="cl-wrapper">
   <div className="cl-page">
     <Header mainTitle="Cole Geerts" />
-    <div className="cl-page-content">
-      <h2>What I do...</h2>
+    <div className="cl-page-content page-work">
+      <h2 className="work-title">What I do</h2>
+      <p className="work-body-text">
+        Though my tech focuses change from week to week (or at least it feels like it when you program), I thought it apt to talk a little more here about the languages, frameworks, and libraries that enable me to do the work that I do.
+        Across my dev journey I've mostly done web work! So HTML, CSS, and JS are first! But I hope you'll see that my penchant for learning crosses all types of programming.
+      </p>
     </div>
   </div>
   <Footer />
