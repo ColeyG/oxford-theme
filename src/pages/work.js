3118211634ae026d17e165480485794c120f0cfd
ColeyG <cjgeerts@gmail.com>
2020-07-04 00:18:07 -0400

    About Copy

--- src/pages/work.js
+++ src/pages/work.js
@@ -19,13 +19,30 @@ export default () => (<div className="cl-wrapper">
           <div className="work-card-image">
             <HeroCard bgImage={'optimized/autumn.jpeg'} />
           </div>
-          <p className="work-card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi maiores numquam, quaerat assumenda consequatur, molestiae ducimus quas iste repellendus ex facilis hic eum, doloribus porro laboriosam. Consectetur explicabo id architecto.</p>
+          <p className="work-card-text">
+            Javascript is my bread and butter. Node rules and I love the React Ecosystem!
+            This site is <a href="https://github.com/ColeyG/oxford-theme" target="_blank" rel="noopener noreferrer" className="cl-a-secondary">built with love using Gatsby</a>.
+            I definitely believe in the "JS Everywhere" stack and I never hesitate to choose JS when it is right for the project. I've also had experience with Vue, Pupeteer, and jQuery to
+            name more big players in the JS field.
+          </p>
         </div>
         <div className="work-card-row">
           <div className="work-card-image">
             <HeroCard bgImage={'optimized/autumn.jpeg'} />
           </div>
-          <p className="work-card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi maiores numquam, quaerat assumenda consequatur, molestiae ducimus quas iste repellendus ex facilis hic eum, doloribus porro laboriosam. Consectetur explicabo id architecto.</p>
+          <p className="work-card-text">
+            Professionally, PHP has been a cornerstone language for me. From frameworks like Laravel, Drupal, and even Magento, I've had experience in most of the big names.
+            Deploying and maintaining large scale PHP applications has made up the most of my career. PHP has proven maturity and stability when handling a wide range of web hurdles.
+          </p>
+        </div>
+        <div className="work-card-row">
+          <div className="work-card-image">
+            <HeroCard bgImage={'optimized/autumn.jpeg'} />
+          </div>
+          <p className="work-card-text">
+            Styling in CSS is great, but it goes so much deeper. Creating maintainable, precise stylesheets through preprocessors like Sass or Less is an important art.
+            Whatever the styling toolchain, I strive to create efficient and good looking web apps. The first paint is so important on the web, so a lot of work goes into making it right.
+          </p>
         </div>
       </div>
     </div>
