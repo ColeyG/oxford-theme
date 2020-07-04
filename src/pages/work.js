5490bf4e2b727cf7ba157f23f3245cfb6ddc28e0
ColeyG <cjgeerts@gmail.com>
2020-07-03 23:26:30 -0400

    Verbiage

--- src/pages/work.js
+++ src/pages/work.js
@@ -12,7 +12,7 @@ export default () => (<div className="cl-wrapper">
         Though my tech focuses change from week to week (or at least it feels like it when you program),
         I thought it apt to talk a little more here about the languages, frameworks, and libraries that enable me to do the work that I do.
         Across my dev journey I've mostly done web work! So HTML, CSS, and JS are first!
-        But I hope you'll see that my penchant for learning crosses all types of programming.
+        But I hope you'll find that my penchant for learning crosses all types of programming.
       </p>
       <div className="work-card-con">
         <div className="work-card-row">
