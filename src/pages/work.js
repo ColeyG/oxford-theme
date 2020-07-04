b1556147592d33c4e07e81846d70639889d02495
ColeyG <cjgeerts@gmail.com>
2020-07-04 00:47:39 -0400

    About Copy

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import Header from '../components/header';
 import HeroCard from '../components/hero-card';
@@ -42,6 +43,28 @@ export default () => (<div className="cl-wrapper">
           <p className="work-card-text">
             Styling in CSS is great, but it goes so much deeper. Creating maintainable, precise stylesheets through preprocessors like Sass or Less is an important art.
             Whatever the styling toolchain, I strive to create efficient and good looking web apps. The first paint is so important on the web, so a lot of work goes into making it right.
+          </p>
+        </div>
+        <div className="work-card-row">
+          <div className="work-card-image">
+            <HeroCard bgImage={'optimized/autumn.jpeg'} />
+          </div>
+          <p className="work-card-text">
+            While I've never applied them professionally, my explorations of C &amp; C++ have been instrumental in my understanding of programming. I love experimenting with
+            making small programs like my <a href="https://github.com/ColeyG/coles-gba-demo" target="_blank" rel="noopener noreferrer" className="cl-a-tertiary">Gameboy Advance Demo</a>.
+            Experience in these languages has imparted a love of types and therefore typescript as well. Creating boutique, elegant software solutions means knowing a whole lot
+            more about the <em>variables</em> in play. On the topic of compiled languages, <a href="https://www.rust-lang.org/" target="_blank" rel="noopener noreferrer" className="cl-a-secondary">Rust</a> has also caught my eye.
+          </p>
+        </div>
+        <div className="work-card-row">
+          <div className="work-card-image">
+            <HeroCard bgImage={'optimized/autumn.jpeg'} />
+          </div>
+          <p className="work-card-text">
+            While in most situations where an <a href="https://en.wikipedia.org/wiki/Interpreted_language#:~:text=An%20interpreted%20language%20is%20a,program%20into%20machine%2Dlanguage%20instructions." target="_blank" rel="noopener noreferrer" className="cl-a-secondary">interpreted language</a> would be appropriate,
+            JS would be my first choice, Python occupies a special cubby in my heart. For data-science applications
+            it is completely out-lapping the other options, and I eagerly await each opportunity to use it.
+            Here is <a href="https://github.com/ColeyG/python-reddit-scraper" target="_blank" rel="noopener noreferrer" className="cl-a-primary">a simple web scraper</a> written in Python 3.
           </p>
         </div>
       </div>
