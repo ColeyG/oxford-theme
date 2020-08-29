72550860ec02f1a94aab7e1296d0adcc22b931bf
ColeyG <cjgeerts@gmail.com>
2020-08-29 11:58:26 -0400

    Hard Refresh on Logo Tap

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="cl-primary-header">
-        <Link className="head-logo" to="/">
-          <h1>{this.props.mainTitle}</h1>
-        </Link>
-        <nav>
-          <ul>
-            <li><Link to="/about" className="cl-a-primary">About My Work</Link></li>
-            <li><Link to="/resume" className="cl-a-secondary">R&eacute;sum&eacute;</Link></li>
-            <li><Link to="/contact" className="cl-a-tertiary">Contact</Link></li>
-            <li><Link to="/blog" className="cl-a-reverse">Blog</Link></li>
-          </ul>
-        </nav>
-      </header>
-    );
+function Header(props) {
+  function refresh(e) {
+    e.preventDefault();
+
+    window.location.assign('/');
   }
+
+  return (
+    <header className="cl-primary-header">
+      <a className="head-logo" href="/" onClick={refresh}>
+        <h1>{props.mainTitle}</h1>
+      </a>
+      <nav>
+        <ul>
+          <li><Link to="/about" className="cl-a-primary">About My Work</Link></li>
+          <li><Link to="/resume" className="cl-a-secondary">R&eacute;sum&eacute;</Link></li>
+          <li><Link to="/contact" className="cl-a-tertiary">Contact</Link></li>
+          <li><Link to="/blog" className="cl-a-reverse">Blog</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
